119e1749270abfaf0822841ab81aabbf2e1c5ed9
Bruno Minossi <b.minossi@gmail.com>
2020-12-29 01:12:12 -0300

    v0.1

--- buildRepo.sh
+++ buildRepo.sh
@@ -1,39 +1,50 @@
 #!/bin/bash
 
-set -eu
+
 
 getMaxPaginationFromNobdd(){
+	result=$(
 	curl -sf 'http://h1.nobbd.de/' | 
 	tr '"' '\n' |
 	grep '?start' |
-	tr '\n' ' ' |
+	tr "\n" ' ' |
 	awk -F= '{print $NF}'
+	)
+	echo -e "$result" | xargs
 }
 
 generatePaginationToNobddWebsite(){
-	maxPagNobdd=`getMaxPaginationFromNobdd`
-	echo {0..$maxPagNobdd..20} | 
-	tr ' ' '\n'
+	maxPagNobdd=$(getMaxPaginationFromNobdd)
+	
+	interval=$(seq 0 20 $maxPagNobdd) 
+	
+	echo -e "$interval" | tr " "  "\n"
 }
 
 getReportsIdsFromNobdd(){
-	paginationRange=generatePaginationToNobddWebsite
+	paginationRange=$(generatePaginationToNobddWebsite)
 	echo -e "$paginationRange" | 
-	xargs -I@ -P10 curl -sf 'http://h1.nobbd.de/index.php?start=@'
+	xargs -I@ -P10 curl -sf 'http://h1.nobbd.de/index.php?start=@' 
 }
 
 getOnlyHackeroneReportLinks(){
-	resultFromNobdd=`getReportsIdsFromNobdd`
+
+	resultFromNobdd=$(getReportsIdsFromNobdd)
 
 	echo -e "$resultFromNobdd" |
 	tr '")' '\n' | 
-	tr -d "'" | 
+	tr "'" " " | 
 	awk -F '/' '/https:\/\/hackerone.com\/reports/{print $NF}' |
-	sort -u
+	tr -d ' ' |
+	grep -E '^[0-9]{1,}$'
+	anew reportLinksHackerOne
 }
 
 getHackerOneReportJson(){
-	jsonReponse=`echo -e "$allReportsId" | xargs -I@ wget -P jsonReports/ https://hackerone.com/reports/@.json`
+	jsonReponse=$(
+		echo -e "$allReportsId" | xargs -I@ wget -P jsonReports/ "https://hackerone.com/reports/@.json"
+	)
+
 	echo $jsonReponse
 }
 
@@ -43,6 +54,12 @@ main(){
 mkdir jsonReports || 
 echo '[INFO] Json Output Folder already exist. Using It ...'
 
+[[ -f "reportLinksHackerOne" ]] &&
+echo '[INFO] HackerOne report id files found. Using it ...';
+allReportsId=`cat reportLinksHackerOne`
+ getHackerOneReportJson "$allReportsId" ||
+echo '[INFO] Extracting all disclosed id reports from H1'
+#getOnlyHackeroneReportLinks
 allReportsId=`getOnlyHackeroneReportLinks`
 getHackerOneReportJson "$allReportsId"
 }
