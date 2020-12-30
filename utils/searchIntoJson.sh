fac2b20db3dfb67474fc24d81a2cff3c0d88946f
Bruno Minossi <b.minossi@gmail.com>
2020-12-30 01:29:23 -0300

    new functionalities finding json

--- utils/searchIntoJson.sh
+++ utils/searchIntoJson.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
-find -name "*.json" |
-xargs -I@ sh -c "gron "@" 2>&1 |
-grep -qEi '$1 = .*$2.*' && echo @"
+
+helper(){
+	usage="Helping you finding keys and values into JSON \n$(basename "$0") [-h] [-s] [-k] -- 
+	\nwhere:
+	\n-h  show this help text\n-s  search 'key' 'value' into JSON files
+	\n-k  list all keys available
+	\n e.g: ./script search weakness.name ssrf"
+	echo -e $usage;
+	exit
+}
+
+
+rawSearch(){
+	find $(dirname $(pwd)) -name "*.json" |
+	xargs -I@ sh -c "gron "@" 2>&1 |
+	grep -qEi '$1 = .*$2.*' && echo @"
+}
+
+main(){
+	case "$1" in
+		search|-s) rawSearch "$2" "$3" ;;
+		keys|-k) cat allKeys; exit ;;
+		help|-h) helper ;;
+		*) echo "Invalid Option. Type ./script help" ;;
+	esac
+}
+
+main "$1" "$2" "$3"
