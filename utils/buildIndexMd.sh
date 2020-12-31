1ec873281d942b241bfca5caa760e7f95652767d
Bruno Minossi <b.minossi@gmail.com>
2020-12-31 10:40:27 -0300

    new version of buildIndexMd

--- utils/buildIndexMd.sh
+++ utils/buildIndexMd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 dir=$(dirname "$1")
 cat "$1" |
-jq -r '"# Title\n"+.title,"# URL \n"+.url,"# Reporter \n"+.reporter.username+"\n"' | anew "$dir"/index.md
+jq -r '"### Title\n"+.title,"#### URL \n"+.url,"#### Severity score",.severity.score,"#### Reporter \n"+.reporter.username+"\n### Bounty paid",.formatted_bounty,"\n\n---\n\n"' >> "$dir"/index.md
